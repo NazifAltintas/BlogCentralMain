a4a34a5d2f5b6304ea4e9ee41e9f14ebb80e8bb3
Your Name <you@example.com>
2022-05-25 13:31:50 +0200

    Initial commit

---
+++ src/test/java/com/example/blogcentralmain/BlogCentralMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogcentralmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCentralMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
