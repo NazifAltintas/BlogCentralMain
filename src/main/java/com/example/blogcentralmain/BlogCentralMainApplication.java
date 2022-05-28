32d45e49e03037fdf04c2280fd5a2423790845a8
EmrahYYY <Emrah.Yavuz@student.intecbrussel.be>
2022-05-28 13:08:25 +0200

    chanching from emrah

--- src/main/java/com/example/blogcentralmain/BlogCentralMainApplication.java
+++ src/main/java/com/example/blogcentralmain/BlogCentralMainApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogCentralMainApplication {
 
     //main class
+    // main classs
     
     public static void main(String[] args) {
         SpringApplication.run(BlogCentralMainApplication.class, args);
