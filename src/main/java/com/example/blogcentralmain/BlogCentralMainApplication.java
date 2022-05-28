b2986d983ff507c0e97e2930dc6a71cf648e2780
EmrahYYY <Emrah.Yavuz@student.intecbrussel.be>
2022-05-28 13:27:00 +0200

    from emrah

--- src/main/java/com/example/blogcentralmain/BlogCentralMainApplication.java
+++ src/main/java/com/example/blogcentralmain/BlogCentralMainApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogCentralMainApplication {
 
     //main class
-    // main classs
+    // some differences from emrah
     
     public static void main(String[] args) {
         SpringApplication.run(BlogCentralMainApplication.class, args);
