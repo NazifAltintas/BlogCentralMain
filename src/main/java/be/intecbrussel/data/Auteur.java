db4afca6cf2ae09f11b49c96a8a611df9d3f67f5
EmrahYYY <Emrah.Yavuz@student.intecbrussel.be>
2022-05-30 16:09:41 +0200

    from emrah

--- src/main/java/be/intecbrussel/data/Auteur.java
+++ src/main/java/be/intecbrussel/data/Auteur.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
+
 public class Auteur {
 
     @Id
@@ -29,6 +30,46 @@ public class Auteur {
         this.email = email;
         this.password = password;
         this.name = name;
+        this.blogs = blogs;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Blog> getBlogs() {
+        return blogs;
+    }
+
+    public void setBlogs(List<Blog> blogs) {
         this.blogs = blogs;
     }
 }
